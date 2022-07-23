59193a6d4a9fadc76ac3d42f5e2595e52f1ca418
Kamil Kalfas <kamilkalfas@gmail.com>
2022-07-23 10:20:01 +0200

    Updated dependencies and gradle

--- app/src/main/java/com/kkalfas/sample/posts/presentation/PostsActivity.kt
+++ app/src/main/java/com/kkalfas/sample/posts/presentation/PostsActivity.kt
@@ -6,11 +6,12 @@ import androidx.lifecycle.ViewModelProviders
 import com.kkalfas.sample.commonui.BaseActivity
 import com.kkalfas.sample.posts.R
 import com.kkalfas.sample.posts.databinding.ActivityPostsBinding
-import dagger.android.AndroidInjection
 import javax.inject.Inject
 
 class PostsActivity : BaseActivity() {
     override val layoutId = R.layout.activity_posts
+
+    private lateinit var binding: ActivityPostsBinding
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -21,11 +22,10 @@ class PostsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding : ActivityPostsBinding = bind()
+        binding = bind()
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.activityPostsView.lifecycleOwner = this
         viewModel.loadPost()
     }
 }
-
